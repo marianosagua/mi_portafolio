f330c27b693b00fa1f4f3608ddcecc9788efd1cb
MarianoSagua <marianosagua4343@gmail.com>
2023-10-06 23:07:07 -0300

    Commit 13

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -11,7 +11,7 @@ export const ContactPage = () => {
 
       <div className="contact__links">
         <a
-          href="https://drive.google.com/file/d/1MO8xpXka3ReIi_Y3B17MzgmzmCw_WQdH/view?usp=sharing"
+          href="https://drive.google.com/file/d/1tB8_auGnJfKC5DfuZ0FTHtYMKFpmusC1/view?usp=sharing"
           target="_blanck"
         >
           Curriculum
